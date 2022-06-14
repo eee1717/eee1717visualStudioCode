933fb5f333a4e0d86c51efaa81593d92fcd61eec
eee1717 <eee1717@naver.com>
2022-06-14 17:30:28 +0900

    06-14  banner 관련수업진행

---
+++ FrontEnd/20JS-Layout/JS/navfix.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(window).on('scroll',function(){
+        scroll_top=$(this).scrollTop();
+        console.log(scroll_top);
+        if(scroll_top>=50){
+            $('nav')
+            .css('position','fixed')
+            .css('width','100%')
+            .css('left','0px')
+            .css('top','0px')
+            .css('z-index','900');
+            $('body').css('magin-top','110px')
+        }
+        else{
+            $('nav')
+            .css('position','relative')
+        }
+    })
+})
